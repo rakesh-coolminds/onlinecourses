95da0ce9d089d6390e0a80c73d827fd6fb11641f
rakesh-coolminds <73598608+rakesh-coolminds@users.noreply.github.com>
2020-10-29 10:38:27 +0530

    Add files via upload

--- src/main/java/com/onlinecourses/model/PriceComponentValue.java
+++ src/main/java/com/onlinecourses/model/PriceComponentValue.java
@@ -4,7 +4,7 @@ import javax.persistence.*;
 import java.io.Serializable;
 import java.math.BigDecimal;
 
-@Table(name = "price_component_vaue")
+@Table(name = "price_component_value")
 @Entity
 public class PriceComponentValue implements Serializable {
     private static final long serialVersionUID = 1L;
